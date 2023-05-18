311434a96bf5fc75195c4580072776a8cde1a3e2
Rhuan Gabriel <rhuan.santos@db1.com.br>
2023-05-18 11:57:06 -0300

    change fonts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
-import { VitePluginFonts } from 'vite-plugin-fonts';
 import { VitePWA } from 'vite-plugin-pwa';
 
+import Unfonts from 'unplugin-fonts/vite';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePluginFonts({
+    Unfonts({
       google: {
         families: ['Inter:wght@400;500;700']
       }
